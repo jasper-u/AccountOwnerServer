240b384ce663d360256b5eb76995ac7fe736c8ab
Jasper Uitvlugt <jasper.uitvlugt.bee@outlook.com>
2022-02-08 17:17:56 +0100

    Fixed controller

--- AccountOwnerServer/WeatherForecast.cs
+++ AccountOwnerServer/WeatherForecast.cs
@@ -15,25 +15,7 @@ namespace AccountOwnerServer
 
         public string Summary { get; set; }
 
-        [ApiController]
-        [Route("[controller]")]
-        public class WeatherForecastController : ControllerBase
-        {
-            private readonly ILoggerManager _logger;
-            public WeatherForecastController(ILoggerManager logger)
-            {
-                _logger = logger;
-            }
-            [HttpGet]
-            public IEnumerable<string> Get()
-            {
-                _logger.LogInfo("Here is info message from the controller.");
-                _logger.LogDebug("Here is debug message from the controller.");
-                _logger.LogWarn("Here is warn message from the controller.");
-                _logger.LogError("Here is error message from the controller.");
-                return new string[] { "value1", "value2" };
-            }
-        }
+     
     }
 
 }
